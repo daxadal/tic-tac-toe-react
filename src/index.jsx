d53a37679821d8a4798fefeb30c0cdc1d8bf3eb7
egarciadececa <e.garciadececa@gmail.com>
2022-07-08 11:18:07 +0200

    Add intermediate Root component App

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Game from "./Game";
+import App from "./App";
 
 import "./index.css";
 
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<App />);
