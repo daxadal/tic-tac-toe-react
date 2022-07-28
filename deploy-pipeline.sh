78a8f065598e1770ed1effcbd242dde03bdc5043
egarciadececa <e.garciadececa@gmail.com>
2022-07-28 13:58:52 +0200

    change key name

--- deploy-pipeline.sh
+++ deploy-pipeline.sh
@@ -2,12 +2,12 @@
 set -e
 
 echo "Deploy $CI_COMMIT_REF_NAME. Triggered by $CI_PIPELINE_SOURCE"
-chmod 400 key.pem
+chmod 400 server_key
 
 echo " === Creating directory... === "
-ssh -o StrictHostKeyChecking=no -i key.pem $SERVER_USER@$SERVER_IP \
+ssh -o StrictHostKeyChecking=no -i server_key $SERVER_USER@$SERVER_IP \
   "mkdir $SERVER_LOCATION -p"
 
 echo " === Starting secure copy... === "
-rsync -e "ssh -o StrictHostKeyChecking=no -i key.pem" -r \
+rsync -e "ssh -o StrictHostKeyChecking=no -i server_key" -r \
   build package.json $SERVER_USER@$SERVER_IP:$SERVER_LOCATION
